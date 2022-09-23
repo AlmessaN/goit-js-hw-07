7c31c33913f6082c6dc977a095bb7de8daf7d016
Stepan Almes <shtukatulk@gmail.com>
2022-09-23 22:29:27 +0300

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+const render = galleryItems.map(item => `<div class="gallery__item">
+<a class:"gallery__link" href="${item.original}">
+<img class: "gallery__image" src="${item.preview}"
+data-source="${item.original}"
+alt= "${item.description}"/></a></div>`).join(" ");
+
+galleryBox.insertAdjacentHTML("beforeend", render)
+
+galleryBox.addEventListener("click", onClick);
+
+function original(e) {
+e.preventDefault()
+if(e.target === e.curentTarget) return;
+
+const currentImage = e.target;
+console.log(`"Current image:${currentImage.dataset.source}"`);
+const instance = basicLightbox.create(
+    `<div class: "modal"> <img src= "${currentImage.dataset.source}"/>
+</div>`)
+instance.show()
+}
